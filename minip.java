6ce2075a8957ba051edc07f31da9c49a17ae609e
hjangir080 <112872382+hjangir080@users.noreply.github.com>
2023-11-05 10:58:07 +0530

    Update minip.java

--- minip.java
+++ minip.java
@@ -1,1 +1,25 @@
-hgjhvvb
+import java.util.*;
+
+class Vehicle{
+	String numPlate;
+}
+class Car extends Vehicle{
+	String modelC;
+}
+class Bike extends Vehicle{
+	String modelB;
+}
+class Truck extends Vehicle{
+	String modelT;
+}
+
+class Building{
+	int floor;
+	int slot;
+}
+
+class OOP{
+	public static void main(String args[]){
+		
+	}
+}

88c32da54afd5a0f68236a22b90bbf6c432b555f
hjangir080 <112872382+hjangir080@users.noreply.github.com>
2023-11-05 12:50:09 +0530

    Update minip.java

--- minip.java
+++ minip.java
@@ -46,121 +46,78 @@ class ParkingSlot {
     }
 }
 
-class ParkedVehicle {
-    Vehicle vehicle;
-    int entryTime;
-    int slotNumber;
-
-    ParkedVehicle(Vehicle vehicle, int entryTime, int slotNumber) {
-        this.vehicle = vehicle;
-        this.entryTime = entryTime;
-        this.slotNumber = slotNumber;
-    }
-}
-
 class ParkingFloor {
     private ParkingSlot[] carSlots;
     private ParkingSlot[] bikeSlots;
     private ParkingSlot[] truckSlots;
-    private ParkedVehicle[] parkedVehicles;
 
-    ParkingFloor(int carSlotsCount, int bikeSlotsCount, int truckSlotsCount) {
-        carSlots = new ParkingSlot[carSlotsCount];
-        bikeSlots = new ParkingSlot[bikeSlotsCount];
-        truckSlots = new ParkingSlot[truckSlotsCount];
-        parkedVehicles = new ParkedVehicle[carSlotsCount + bikeSlotsCount + truckSlotsCount];
+    ParkingFloor() {
+        carSlots = new ParkingSlot[12];
+        bikeSlots = new ParkingSlot[15];
+        truckSlots = new ParkingSlot[3];
 
-        for (int i = 0; i < carSlotsCount; i++) {
+        for (int i = 0; i < 12; i++) {
             carSlots[i] = new ParkingSlot();
         }
 
-        for (int i = 0; i < bikeSlotsCount; i++) {
+        for (int i = 0; i < 15; i++) {
             bikeSlots[i] = new ParkingSlot();
         }
 
-        for (int i = 0; i < truckSlotsCount; i++) {
+        for (int i = 0; i < 3; i++) {
             truckSlots[i] = new ParkingSlot();
         }
     }
 
-    void parkVehicle(Vehicle vehicle, String type, int entryTime) throws Exception {
-        int slotNumber = -1;
-        switch (type) {
-            case "c":
-                slotNumber = parkInSlots(carSlots, vehicle, "Car");
-                break;
-            case "b":
-                slotNumber = parkInSlots(bikeSlots, vehicle, "Bike");
-                break;
-            case "t":
-                slotNumber = parkInSlots(truckSlots, vehicle, "Truck");
-                break;
-            default:
-                throw new Exception("Invalid vehicle type.");
-        }
-        if (slotNumber != -1) {
-            parkedVehicles[slotNumber] = new ParkedVehicle(vehicle, entryTime, slotNumber);
-            System.out.println(vehicle.model + " parked successfully in Slot " + slotNumber);
+    void parkCar(Car car, int slotNumber) throws Exception {
+        parkInSlot(carSlots, car, slotNumber, "Car");
+    }
+
+    void parkBike(Bike bike, int slotNumber) throws Exception {
+        parkInSlot(bikeSlots, bike, slotNumber, "Bike");
+    }
+
+    void parkTruck(Truck truck, int slotNumber) throws Exception {
+        parkInSlot(truckSlots, truck, slotNumber, "Truck");
+    }
+
+    private void parkInSlot(ParkingSlot[] slots, Vehicle vehicle, int slotNumber, String vehicleType) throws Exception {
+        if (slotNumber >= 0 && slotNumber < slots.length && !slots[slotNumber].isOccupied()) {
+            slots[slotNumber].parkVehicle(vehicle);
+            System.out.println(vehicleType + " parked successfully in Slot " + slotNumber);
         } else {
-            throw new Exception("No available slots for the vehicle.");
+            throw new Exception("Invalid slot number or slot already occupied for " + vehicleType + ".");
         }
     }
 
-    private int parkInSlots(ParkingSlot[] slots, Vehicle vehicle, String vehicleType) {
-        for (int i = 0; i < slots.length; i++) {
-            if (!slots[i].isOccupied()) {
-                slots[i].parkVehicle(vehicle);
-                return i;
-            }
+    Vehicle retrieveVehicle(int slotNumber, int exitTime) {
+        ParkingSlot[] slots = null;
+        String vehicleType = "";
+
+        if (slotNumber >= 0 && slotNumber < carSlots.length && carSlots[slotNumber].isOccupied()) {
+            slots = carSlots;
+            vehicleType = "Car";
+        } else if (slotNumber >= 0 && slotNumber < bikeSlots.length && bikeSlots[slotNumber].isOccupied()) {
+            slots = bikeSlots;
+            vehicleType = "Bike";
+        } else if (slotNumber >= 0 && slotNumber < truckSlots.length && truckSlots[slotNumber].isOccupied()) {
+            slots = truckSlots;
+            vehicleType = "Truck";
         }
-        return -1; // No available slots
-    }
 
-    void retrieveVehicle(int slotNumber, int exitTime) {
-        if (slotNumber >= 0 && slotNumber < parkedVehicles.length && parkedVehicles[slotNumber] != null) {
-            ParkedVehicle parkedVehicle = parkedVehicles[slotNumber];
-            int entryTime = parkedVehicle.entryTime;
-            Vehicle vehicle = parkedVehicle.vehicle;
-
-            int hours = (exitTime - entryTime) / 100; // Calculate hours difference in military format
-            int payment = calculatePayment(hours, vehicle);
-
-            System.out.println("Vehicle retrieved successfully.");
-            System.out.println("Vehicle Details - Num Plate: " + vehicle.numPlate + ", Model: " + vehicle.model);
-            System.out.println("Entry Time: " + entryTime + ", Exit Time: " + exitTime);
-            System.out.println("Total Parking Duration: " + hours + " hours");
-            System.out.println("Payment Fee: $" + payment);
+        if (slots != null) {
+            Vehicle vehicle = slots[slotNumber].retrieveVehicle();
+            int hours = (exitTime - 1500) / 100; // Assuming entry time is always before 1500 hours
+            int basePrice = (vehicleType.equals("Bike")) ? 20 : ((vehicleType.equals("Car")) ? 50 : 100);
+            int additionalFee = (hours <= 4) ? (hours * 20) : 1000;
+            int totalPayment = basePrice + additionalFee;
+            System.out.println(vehicleType + " retrieved successfully from Slot " + slotNumber);
+            System.out.println("Total Payment: Rs. " + totalPayment);
+            return vehicle;
         } else {
             System.out.println("Invalid slot number or no vehicle parked at this slot.");
+            return null;
         }
-    }
-
-    private int calculatePayment(int hours, Vehicle vehicle) {
-        int basePrice = 0;
-        int hourlyRate = 0;
-        int additionalHourlyRate = 0;
-
-        if (vehicle instanceof Bike) {
-            basePrice = 20;
-            hourlyRate = 20;
-            additionalHourlyRate = 1000;
-        } else if (vehicle instanceof Car) {
-            basePrice = 50;
-            hourlyRate = 50;
-            additionalHourlyRate = 1000;
-        } else if (vehicle instanceof Truck) {
-            basePrice = 100;
-            hourlyRate = 100;
-            additionalHourlyRate = 1000;
-        }
-
-        int payment = basePrice + (hours * hourlyRate);
-
-        if (hours > 4) {
-            payment += ((hours - 4) * additionalHourlyRate);
-        }
-
-        return payment;
     }
 }
 
@@ -170,23 +127,28 @@ class ParkingLot {
     ParkingLot(int floorsCount) {
         floors = new ParkingFloor[floorsCount];
         for (int i = 0; i < floorsCount; i++) {
-            floors[i] = new ParkingFloor(12, 15, 3);
+            floors[i] = new ParkingFloor();
         }
     }
 
-    void parkVehicle(Vehicle vehicle, String type, int floorNumber, int entryTime) throws Exception {
-        if (floorNumber >= 0 && floorNumber < floors.length) {
-            floors[floorNumber].parkVehicle(vehicle, type, entryTime);
-        } else {
-            throw new Exception("Invalid floor number.");
-        }
+    void parkCar(Car car, int floorNumber, int slotNumber) throws Exception {
+        floors[floorNumber].parkCar(car, slotNumber);
     }
 
-    void retrieveVehicle(int floorNumber, int slotNumber, int exitTime) {
+    void parkBike(Bike bike, int floorNumber, int slotNumber) throws Exception {
+        floors[floorNumber].parkBike(bike, slotNumber);
+    }
+
+    void parkTruck(Truck truck, int floorNumber, int slotNumber) throws Exception {
+        floors[floorNumber].parkTruck(truck, slotNumber);
+    }
+
+    Vehicle retrieveVehicle(int floorNumber, int slotNumber, int exitTime) {
         if (floorNumber >= 0 && floorNumber < floors.length) {
-            floors[floorNumber].retrieveVehicle(slotNumber, exitTime);
+            return floors[floorNumber].retrieveVehicle(slotNumber, exitTime);
         } else {
             System.out.println("Invalid floor number.");
+            return null;
         }
     }
 }
@@ -200,68 +162,77 @@ public class OopProject {
 
         while (true) {
             System.out.println("Choose an option:");
-            System.out.println("1. Park a vehicle");
-            System.out.println("2. Retrieve a vehicle");
-            System.out.println("3. Exit");
+            System.out.println("1. Park a car");
+            System.out.println("2. Park a bike");
+            System.out.println("3. Park a truck");
+            System.out.println("4. Retrieve a vehicle");
+            System.out.println("5. Exit");
             int choice = sc.nextInt();
             sc.nextLine(); // Consume the newline character after reading the choice
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter type of vehicle: (c for car, t for truck, b for bike)");
-                    String type = sc.nextLine();
-
-                    System.out.println("Enter Num plate:");
-                    String numPlate = sc.nextLine();
-                    System.out.println("Enter Model:");
-                    String model = sc.nextLine();
-
-                    System.out.println("Enter Entry Time (in military hours format, e.g., 1500):");
-                    int entryTime = sc.nextInt();
-
-                    System.out.println("Enter Floor Number (0, 1, 2):");
-                    int floorNumber = sc.nextInt();
-
+                    System.out.println("Enter Num plate for car:");
+                    String carNumPlate = sc.nextLine();
+                    System.out.println("Enter Model for car:");
+                    String carModel = sc.nextLine();
+                    Car car = new Car(carNumPlate, carModel);
                     try {
-                        Vehicle vehicle = null;
-                        switch (type) {
-                            case "c":
-                                vehicle = new Car(numPlate, model);
-                                break;
-                            case "b":
-                                vehicle = new Bike(numPlate, model);
-                                break;
-                            case "t":
-                                vehicle = new Truck(numPlate, model);
-                                break;
-                            default:
-                                System.out.println("Invalid vehicle type.");
-                                continue;
-                        }
-
-                        // Park the vehicle with entry time
-                        parkingLot.parkVehicle(vehicle, type, floorNumber, entryTime);
-                        System.out.println("Vehicle parked successfully.");
+                        System.out.println("Enter Floor Number (0, 1, 2):");
+                        int floorNumber = sc.nextInt();
+                        System.out.println("Enter Slot Number (0-11):");
+                        int slotNumber = sc.nextInt();
+                        parkingLot.parkCar(car, floorNumber, slotNumber);
                     } catch (Exception e) {
                         System.out.println("Error: " + e.getMessage());
                     }
                     break;
 
                 case 2:
-                    System.out.println("Enter Floor Number (0, 1, 2):");
-                    int floorNum = sc.nextInt();
-
-                    System.out.println("Enter Slot Number:");
-                    int slotNumber = sc.nextInt();
-
-                    System.out.println("Enter Exit Time (in military hours format, e.g., 1700):");
-                    int exitTime = sc.nextInt();
-
-                    // Retrieve the vehicle and calculate payment
-                    parkingLot.retrieveVehicle(floorNum, slotNumber, exitTime);
+                    System.out.println("Enter Num plate for bike:");
+                    String bikeNumPlate = sc.nextLine();
+                    System.out.println("Enter Model for bike:");
+                    String bikeModel = sc.nextLine();
+                    Bike bike = new Bike(bikeNumPlate, bikeModel);
+                    try {
+                        System.out.println("Enter Floor Number (0, 1, 2):");
+                        int floorNumber = sc.nextInt();
+                        System.out.println("Enter Slot Number (0-14):");
+                        int slotNumber = sc.nextInt();
+                        parkingLot.parkBike(bike, floorNumber, slotNumber);
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
                     break;
 
                 case 3:
+                    System.out.println("Enter Num plate for truck:");
+                    String truckNumPlate = sc.nextLine();
+                    System.out.println("Enter Model for truck:");
+                    String truckModel = sc.nextLine();
+                    Truck truck = new Truck(truckNumPlate, truckModel);
+                    try {
+                        System.out.println("Enter Floor Number (0, 1, 2):");
+                        int floorNumber = sc.nextInt();
+                        System.out.println("Enter Slot Number (0-2):");
+                        int slotNumber = sc.nextInt();
+                        parkingLot.parkTruck(truck, floorNumber, slotNumber);
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Enter Floor Number (0, 1, 2):");
+                    int floorNumber = sc.nextInt();
+                    System.out.println("Enter Slot Number:");
+                    int slotNumber = sc.nextInt();
+                    System.out.println("Enter Exit Time (in military hours):");
+                    int exitTime = sc.nextInt();
+                    parkingLot.retrieveVehicle(floorNumber, slotNumber, exitTime);
+                    break;
+
+                case 5:
                     System.out.println("Exiting the parking management system. Goodbye!");
                     System.exit(0);
                     break;
